e5c7c9d59197b55f77005c2f769459427fca23ca
Dirghayu Joshi <dirghayujoshi48@gmail.com>
2026-01-20 18:09:10 -0500

    Changed the package.json, install newer packages and ran locally in dev.

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -13,6 +13,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 export default nextConfig;
